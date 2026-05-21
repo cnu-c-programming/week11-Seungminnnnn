a88446725350d646e7792a7771fac19fe9f2c370
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-21 18:53:33 +0900

    Fix error message for null pointer check
    Updated error message from 'NULL pointer' to 'Null pointer'.

--- code/3-1.c
+++ code/3-1.c
@@ -8,7 +8,7 @@ int mul(int a, int b) {return a * b;}
 
 int compute(int a, int b, BinOp op) {
     if (op == NULL) {
-        printf("NULL pointer\n");
+        printf("Null pointer\n");
         return -1;
     }
 
